01240de1c3a14cb01a355672d2c3cdab1d78ec0e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-11-15 18:26:59 -0500

    hooked up res.render to "user" template to display name and image

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('users', {
+        user: { 
+            name: req.user.displayName, 
+            image: req.user._json.image.url
+        }
+    });
 });
-
 module.exports = router;
