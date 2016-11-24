9c97544d8c54d59db158295255140bdf3b9ddee6
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-11-24 09:05:15 -0500

    redirect function and get method (to render the name and image for the user) for user route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var router = express.Router();
+
+router.use('/', function(req, res, next){
+    if(!req.user) {
+        res.redirect('/');
+    }
+    next();
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('users', {
         user: { 
             name: req.user.displayName, 
-            image: req.user._json.image.url
+            image: req.user.image
         }
     });
 });
