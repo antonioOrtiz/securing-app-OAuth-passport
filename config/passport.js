f150bbfa6815cdedf83c521164c922594238aa04
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-12-08 10:18:46 -0500

    fix to require statement/invocation to google strategy and brought in twitter require statement/invovation

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,21 @@
 var passport = require('passport');
 
 
-module.exports = function(app) {
+module.exports = function (app) {
+
     app.use(passport.initialize());
     app.use(passport.session());
 
-    passport.serializeUser(function(user, done) {
-        done(null, user);
+    passport.serializeUser(function (user, done) {
+        done(null, user)
     });
 
-    passport.deserializeUser(function(user, done) {
-
+    passport.deserializeUser(function (user, done) {
         done(null, user);
     });
-
-require('./strate     gies/google.strategy')();
+    
+    require('./strategies/google.strategy')();
+    require('./strategies/twitter.strategy')();
+    // require('./strategies/facebook.strategy')();
 
 };
