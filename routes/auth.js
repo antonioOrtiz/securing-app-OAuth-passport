ca58f8a56e53e053757f09f317372620e78cfdd2
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-12-08 10:14:24 -0500

    commented out facebook authenticate route

--- routes/auth.js
+++ routes/auth.js
@@ -1,28 +1,36 @@
-var express = require('express'),
-    passport = require('passport'),
-    router = express.Router();
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
 
 
 router.route('/google/callback')
-      .get(passport.authenticate('google', {
-            successRedirect: '/users',
-            failure: '/error/'
-      }));
+    .get(passport.authenticate('google', {
+        successRedirect: '/users/',
+        failure: '/error/'
+    }));
 
-router.route('/google') 
+router.route('/google')
     .get(passport.authenticate('google', {
         scope: ['https://www.googleapis.com/auth/userinfo.profile',
-         'https://www.googleapis.com/auth/userinfo.email']
+            'https://www.googleapis.com/auth/userinfo.email']
     }));
-
-router.route('/twitter/callback') 
+router.route('/twitter/callback')
     .get(passport.authenticate('twitter', {
-        successRedirect: '/users',
-            failure: '/error/'
+        successRedirect: '/users/',
+        failure: '/error/'
     }));
 
-router.route('/twitter') 
+router.route('/twitter')
     .get(passport.authenticate('twitter'));
 
 
+// router.route('/facebook')
+//     .get(passport.authenticate('facebook', {
+//         scope: ['email']
+//     }));
+// router.route('/facebook/callback')
+//     .get(passport.authenticate('facebook', {
+//         successRedirect: '/users',
+//         failureRedirect: '/error'
+//     }));
 module.exports = router;
