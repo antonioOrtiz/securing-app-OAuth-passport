ed746b193a06cc7fce6a70b5f7423a4bbaaa65a9
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-12-13 18:31:36 -0500

    passport.js now includes github strategy

--- config/passport.js
+++ config/passport.js
@@ -1,22 +1,24 @@
 var passport = require('passport');
 
 
-module.exports = function (app) {
+module.exports = function(app) {
 
     app.use(passport.initialize());
     app.use(passport.session());
 
-    passport.serializeUser(function (user, done) {
-        done(null, user)
-    });
-
-    passport.deserializeUser(function (user, done) {
+    passport.serializeUser(function(user, done) {
         done(null, user);
     });
-    
+
+    passport.deserializeUser(function(user, done) {
+        done(null, user);
+    });
+
     require('./strategies/google.strategy')();
     require('./strategies/twitter.strategy')();
     require('./strategies/facebook.strategy')();
     require('./strategies/linkedin.strategy')();
+    require('./strategies/github.strategy')();
+
 
 };
