b002efb00110f3f3c7c3925bb7199a2ce9e7ff1c
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-12-12 20:24:28 -0500

    facebook strategy works!

--- config/strategies/facebook.strategy.js
+++ config/strategies/facebook.strategy.js
@@ -2,20 +2,22 @@ var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
 
- module.exports = function() {
+module.exports = function() {
 
-     passport.use(new FacebookStrategy({
-             clientID: '1125753470870888',
-             clientSecret: '2152eb7f3d686459fe5d5224d1f9abc8',
-             callbackURL: 'http://localhost:3000/auth/facebook/callback',
-             profileFields: [ 'email', 'name' ]
-         },
-        function (req, accessToken, refreshToken, profile, done) {
+    passport.use(new FacebookStrategy({
+            clientID: '1125753470870888',
+            clientSecret: '2152eb7f3d686459fe5d5224d1f9abc8',
+            callbackURL: 'http://localhost:3000/auth/facebook/callback',
+            passReqToCallback: true,
+            profileFields: ['id', 'email', 'name', 'displayName'] // HERE!
+
+        },
+        function(req, accessToken, refreshToken, profile, done) {
 
             var user = {};
 
             user.email = profile.emails[0].value;
-            //user.image = profile._json.image.url;
+            // user.image = profile._json.image.url;
             user.displayName = profile.displayName;
 
             user.facebook = {};
