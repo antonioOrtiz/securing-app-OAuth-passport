138ab070c5ef15766ea821796556b34a287260cd
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-12-14 20:38:07 -0500

    refactored twiiter.strategy

--- config/strategies/twitter.strategy.js
+++ config/strategies/twitter.strategy.js
@@ -1,26 +1,27 @@
- var passport = require('passport'),
-     TwitterStrategy = require('passport-twiiter').Strategy;
+var passport = require('passport');
+var TwitterStrategy = require('passport-twitter').Strategy;
 
 
  module.exports = function() {
 
      passport.use(new TwitterStrategy({
-             consumerKey: '8eCNHmmd8zirh7ReEmlSq4pQi',
-             consumerSecret: 'j63anLdeAaIUhSQJJSljJ3L1hGSaqH4fUMpeWYi9X7WWC0Ogjb',
-             callbackURL: 'http://localhost.com/twitter/callback',
+             consumerKey: '9V3uHVokBQ99aMUQYyDKJJCbD',
+             consumerSecret: 'NOOqcOgDKw6fz1mghpBCNzjOEiXBZhHpWRD69JlSw7qCNpUvU0',
+             callbackURL: 'http://localhost:3000/auth/twitter/callback',
              passReqToCallback: true
          },
          function(req, token, tokenSecret, profile, done) {
-             var user = {};
+            var user = {};
+            //user.email = profile.emails[0].value;
+            // user.image = profile._json.image_url;
+            user.image = profile._json.profile_image_url;
+            user.displayName = profile.displayName;
 
-             user.email = profile.emails[0].value;
-             user.image = profile._json.image.url;
-             user.displayName = profile.displayName;
-
-             user.twitter = {};
-             user.twitter.id = profile.id;
-             user.twitter.token = accessToken;
-             done(null, user);
+            user.twitter = {};
+            user.twitter.id = profile.id;
+            user.twitter.token = token;
+             
+            done(null, user);
 
          }
      ));
