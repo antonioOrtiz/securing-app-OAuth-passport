858b5a52f0a6c38d8e2cd8dcec27d9390ab41ecb
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-01-15 10:26:41 -0500

    Learning how to save User to DB

--- config/strategies/google.strategy.js
+++ config/strategies/google.strategy.js
@@ -1,27 +1,41 @@
- var passport = require('passport'),
-     GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var passport = require('passport'),
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+    var User = require('../../models/userModel');
 
 
- module.exports = function() {
+module.exports = function() {
 
-     passport.use(new GoogleStrategy({
-             clientID: '28540992152-8ec10ajum8fobgsgubfdgqupgctb747d.apps.googleusercontent.com',
-             clientSecret: 'pebfG-9OQ2r1Jj1pbeVYfu02',
-             callbackURL: 'http://localhost:3000/auth/google/callback'
-         },
-        function (req, accessToken, refreshToken, profile, done) {
+    passport.use(new GoogleStrategy({
+            clientID: '28540992152-8ec10ajum8fobgsgubfdgqupgctb747d.apps.googleusercontent.com',
+            clientSecret: 'pebfG-9OQ2r1Jj1pbeVYfu02',
+            callbackURL: 'http://localhost:3000/auth/google/callback'
+        },
+        function(req, accessToken, refreshToken, profile, done) {
 
-            var user = {};
+                var query = {
+                    'google.id': profile.id
+                };
 
-            user.email = profile.emails[0].value;
-            user.image = profile._json.image.url;
-            user.displayName = profile.displayName;
+            User.findOne(query, function(error, user) {
+                if (user) {
+                    console.log('found');
+                    done(null, user);
+                } else {
+                    console.log('not found');
+                    var user = new User;
+                    user.email = profile.emails[0].value;
+                    user.image = profile._json.image.url;
+                    user.displayName = profile.displayName;
 
-            user.facebook = {};
-            user.facebook.id = profile.id;
-            user.facebook.token = accessToken;
+                    user.google = {};
+                    user.google.id = profile.id;
+                    user.google.token = accessToken;
+                    user.save();
+                    done(null, user);
+                }
+            });
 
-            done(null, user);
+
         }));
 
-}
+};
