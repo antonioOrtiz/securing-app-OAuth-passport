2d3fb30666e954c8f2469d5bd89dedb1788aa549
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-01-16 13:33:18 -0500

    twitter strategy User Model applied

--- config/strategies/twitter.strategy.js
+++ config/strategies/twitter.strategy.js
@@ -1,28 +1,42 @@
 var passport = require('passport');
 var TwitterStrategy = require('passport-twitter').Strategy;
+var User = require('../../models/userModel');
 
 
- module.exports = function() {
 
-     passport.use(new TwitterStrategy({
-             consumerKey: '9V3uHVokBQ99aMUQYyDKJJCbD',
-             consumerSecret: 'NOOqcOgDKw6fz1mghpBCNzjOEiXBZhHpWRD69JlSw7qCNpUvU0',
-             callbackURL: 'http://localhost:3000/auth/twitter/callback',
-             passReqToCallback: true
-         },
-         function(req, token, tokenSecret, profile, done) {
-            var user = {};
-            //user.email = profile.emails[0].value;
-            // user.image = profile._json.image_url;
-            user.image = profile._json.profile_image_url;
-            user.displayName = profile.displayName;
+module.exports = function() {
 
-            user.twitter = {};
-            user.twitter.id = profile.id;
-            user.twitter.token = token;
-             
-            done(null, user);
+    passport.use(new TwitterStrategy({
+            consumerKey: '9V3uHVokBQ99aMUQYyDKJJCbD',
+            consumerSecret: 'NOOqcOgDKw6fz1mghpBCNzjOEiXBZhHpWRD69JlSw7qCNpUvU0',
+            callbackURL: 'http://localhost:3000/auth/twitter/callback',
+            passReqToCallback: true
+        },
+        function(req, token, tokenSecret, profile, done) {
+            var query = {
+                    'twitter.id': profile.id
+                };
 
-         }
-     ));
- };
+            User.findOne(query, function(error, user) {
+                if (user) {
+                    console.log('found');
+                    done(null, user);
+                } else {
+                    console.log('not found');
+                    var user = new User;
+                    // user.email = profile.emails[0].value;
+                    user.image = profile._json.profile_image_url;
+                    user.displayName = profile.displayName;
+
+                    user.twitter = {};
+                    user.twitter.id = profile.id;
+                    user.twitter.token = token;
+
+                    user.save();
+                    done(null, user);
+                }
+            });
+
+        }
+    ));
+};
