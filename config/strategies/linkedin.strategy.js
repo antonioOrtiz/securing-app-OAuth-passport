5c52c5b4ee4e4828c102363be677bf17b33bcf9e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-01-17 10:32:49 -0500

    User model applied to linkedin strategy

--- config/strategies/linkedin.strategy.js
+++ config/strategies/linkedin.strategy.js
@@ -1,5 +1,7 @@
 var passport = require('passport');
 var LinkedInStrategy = require('passport-linkedin').Strategy;
+var User = require('../../models/userModel');
+
 
 
 module.exports = function() {
@@ -7,18 +9,33 @@ module.exports = function() {
     passport.use(new LinkedInStrategy({
             consumerKey: '77z7e2bq34ad0w',
             consumerSecret: 'ajTJfuNTxLnDGlCK',
-            callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+            callbackURL: "http://localhost:3000/auth/linkedin/callback"
         },
-        function(token, tokenSecret, profile, done) {
-            var user = {};
-            // user.email = profile.emails[0].value;
-            // user.image = profile._json.image.url;
-            user.displayName = profile.displayName;
+        function(req, token, tokenSecret, profile, done) {
+            var query = {
+                'linkedin.id': profile.id
+            };
 
-            user.linkedin = {};
-            user.linkedin.id = profile.id;
-            user.linkedin.token = tokenSecret;
-            done(null, user);
+            User.findOne(query, function(error, user) {
+                if (user) {
+                    console.log('found');
+                    done(null, user);
+                } else {
+                    console.log('not found');
+                    var user = new User;
+                    user.email = profile.emails[0].value;
+                    user.image = profile._json.image.url;
+                    user.displayName = profile.displayName;
+
+                    user.linkedin = {};
+                    user.linkedin.id = profile.id;
+                    user.linkedin.token = accessToken;
+                    user.save();
+                    done(null, user);
+                }
+            });
+
+
         }));
 
 };
