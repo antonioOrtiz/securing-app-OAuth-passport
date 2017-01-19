eca56227a19c8c89327aa6fe8168d02385696d3e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-01-19 09:38:09 -0500

    "/" route now checks for req.user.facebook and all associated permissions alloted by token

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var facebook = require('../services/facebook')
+('1125753470870888', '2152eb7f3d686459fe5d5224d1f9abc8');
 
 router.use('/', function(req, res, next){
     if(!req.user) {
@@ -10,9 +12,15 @@ router.use('/', function(req, res, next){
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-    res.render('users', {
-        user: req.user
-    });
+    if (req.user.facebook) {
+        facebook.getImage(req.user.facebook.token, function (results) {
+            /* body... */
+            req.user.facebook.image = results.url;
+            res.render('users', {user: req.user});
+        });
+    } else {
+        res.render('users', {user: req.user});
+    }
 });
 
 module.exports = router;
