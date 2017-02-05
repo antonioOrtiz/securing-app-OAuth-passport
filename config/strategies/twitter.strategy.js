4a2d0121669f4f600618023a6d47341d093d5937
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2017-02-04 21:12:59 -0500

    refactored FB strategy.js

--- config/strategies/twitter.strategy.js
+++ config/strategies/twitter.strategy.js
@@ -12,31 +12,65 @@ module.exports = function() {
             callbackURL: 'http://localhost:3000/auth/twitter/callback',
             passReqToCallback: true
         },
-        function(req, token, tokenSecret, profile, done) {
-            var query = {
+        function (req, token, tokenSecret, profile, done) {
+            if (req.user) {
+                var query = {};
+                if(req.user.google)
+                {
+                    console.log('google');
+                    var query = {
+                        'google.id': req.user.google.id
+                    };
+                } else if(req.user.facebook) {
+                    var query = {
+                        'facebook.id': req.user.facebook.id
+                    };
+                } else if (req.user.github) {
+                    var query = {
+                        'github.id': req.user.github.id
+                    };
+                }
+                User.findOne(query, function (error, user) {
+                    console.log(error);
+                    console.log('user');
+                    if(user) {
+                        user.twitter = {};
+                        user.twitter.id = profile.id;
+                        user.twitter.token = token;
+                        user.twitter.tokenSecret = tokenSecret;
+                        user.save();
+                        done(null, user);
+                    }
+                })
+                
+            } else {
+                var query = {
                     'twitter.id': profile.id
                 };
 
-            User.findOne(query, function(error, user) {
-                if (user) {
-                    console.log('found');
-                    done(null, user);
-                } else {
-                    console.log('not found');
-                    var user = new User;
-                    // user.email = profile.emails[0].value;
-                    user.image = profile._json.profile_image_url;
-                    user.displayName = profile.displayName;
+                User.findOne(query, function (error, user) {
+                    if (user) {
+                        console.log('found');
+                        done(null, user);
+                    } else {
+                        console.log('not found');
+                        var user = new User;
 
-                    user.twitter = {};
-                    user.twitter.id = profile.id;
-                    user.twitter.token = token;
+                        //user.email = profile.emails[0].value;
+                        user.image =
+                            profile._json.profile_image_url;
+                        user.displayName = profile.displayName;
 
-                    user.save();
-                    done(null, user);
-                }
-            });
+                        user.twitter = {};
+                        user.twitter.id = profile.id;
+                        user.twitter.token = token;
+                        user.twitter.tokenSecret = tokenSecret;
 
-        }
-    ));
+                        user.save();
+                        done(null, user);
+                    }
+                })
+            }
+
+        }))
 };
